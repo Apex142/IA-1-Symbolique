63319a210aaded163fe2d45994b77cd3ecfc366a
Apex142 <martin.mehdi.mehdi.lerenard@gmail.com>
2025-02-06 17:23:33 +0100

    Add goal grid representation and update main game logic

--- src/fr/mehdichadi/core/Main.java
+++ src/fr/mehdichadi/core/Main.java
@@ -2,6 +2,8 @@ package fr.mehdichadi.core;
 
 import fr.mehdichadi.file.factory.GameFileReaderFactory;
 import fr.mehdichadi.file.reader.GameFileReader;
+import fr.mehdichadi.game.Direction;
+import fr.mehdichadi.game.Grid;
 import fr.mehdichadi.game.State;
 import fr.mehdichadi.type.FileType;
 
@@ -10,22 +12,26 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // Chemin du fichier de jeu .grid (À modifier selon votre cas)
-        String filePath = "taquin_3x3.grid";
+        String filePath = "src/resources/taquin_3x3.grid";
         FileType fileType = FileType.GRID;
 
         // Lecture du fichier et création de l'état initial
         System.out.println("Loading game state from file: " + filePath);
         GameFileReader fileReader = GameFileReaderFactory.getFileReader(fileType);
-        State initialState = fileReader.readGameState(filePath);
+        Grid initialState = (Grid) fileReader.readGameState(filePath);
 
         System.out.println("Initial State:");
         System.out.println(initialState);
 
-        System.out.println("successors:");
-        List<State> successors = initialState.getSuccessors();
-        for (State successor : successors) {
-            System.out.println(successor);
+        System.out.println("succesor states:");
+        for (State state : initialState.getSuccessors()) {
+            System.out.println(state);
         }
+
+        System.out.println("Goal State :");
+        System.out.println(initialState.toStringGoalGrid());
+
+
 
 
     }
